a97a33a3e6bf6ce56788b79ae9c747c881d01032
e185760 <e185760@ie.u-ryukyu.ac.jp>
2021-12-13 15:43:56 +0900

    os2.1 FileWrite

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let name_string = "YoshitakaYamashita";
+    let nennrei_float = 21;
+
+
+    println!("str is:{}", name_string);
+    println!("flo is:{}", nennrei_float);
+
+}
+
+mod tests{
+    #[test]
+    fn it_works(){
+        assert_eq!(100+100, 200)
+    }
+}
